4ca150b7f8c06ec8c062b4cec5ce3fe98313310a
hsk <1059865352@qq.com>
2025-05-04 20:25:32 +0800

    1.0.2

---
+++ config.js
@@ -0,0 +1,15 @@
+// 敏感配置信息
+export const firebaseConfig = {
+  apiKey: "AIzaSyAWIdKwbQBX5-GTfMwb_El4991OXK-_Bfw",
+  authDomain: "puppy-5443b.firebaseapp.com",
+  databaseURL: "https://puppy-5443b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "puppy-5443b",
+  storageBucket: "puppy-5443b.firebasestorage.app",
+  messagingSenderId: "548254394160",
+  appId: "1:548254394160:web:268692ff52ecf641adcaf5",
+  measurementId: "G-T0X1YL9RXX"
+};
+
+// 登录密码
+export const ADMIN_PASSWORD = "xiaohao";
+export const PUPPY_PASSWORD = "lele";
